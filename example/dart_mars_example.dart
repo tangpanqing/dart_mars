93d2008897cf8bafbb2cc309caaa9b1a164cb6b9
tangpanqing <tangpanqing@qq.com>
2021-06-04 19:17:09 +0800

    sheet

--- example/dart_mars_example.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_mars/dart_mars.dart';
-
-void main(List<String> args) {
-  App.startHttp(args);
-  App.startHttps(args);
-  App.startWebsocket(args);
-}
--- lib/dart_mars.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library dart_mars;
-
-export 'src/App.dart';
-
-// TODO: Export any libraries intended for clients of this package.
