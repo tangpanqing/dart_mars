36473a21d62f0947ddaa76207c01e7490999da23
tangpanqing <tangpanqing@qq.com>
2021-06-05 17:48:34 +0800

    template

--- lib/TemplateHelper.dart
+++ lib/TemplateHelper.dart
@@ -64,16 +64,17 @@ dependencies:
       HttpServer.bind('0.0.0.0', port).then((httpServer) async {
         PrintHelper.p('----Http服务器已经启动 port=' + port.toString());
         await for (HttpRequest request in httpServer) {
-          PrintHelper.p('---------------------------------');
-          PrintHelper.p('----Http请求已接收');
-          PrintHelper.p('request.uri.path = ' + request.uri.path);
-          PrintHelper.p('request.uri.queryParameters = ' +
+          PrintHelper.p('---------------------');
+          PrintHelper.p('----Http请求已接收----');
+          PrintHelper.t('class Server request.uri.path = ' + request.uri.path);
+          PrintHelper.t('class Server request.uri.queryParameters = ' +
               request.uri.queryParameters.toString());
 
           Context ctx = Context(mode:mode, env:env);
           await ctx.handle(request);
           await RouteHelper.handle(ctx);
-          PrintHelper.p('----Http请求已处理');
+          PrintHelper.t('class Server ctx.responseContent = ' + ctx.responseContent);
+          PrintHelper.p('----Http请求已处理----');
         }
       });
     }
@@ -273,6 +274,10 @@ dependencies:
   class PrintHelper {
     static void p(String text) {
       print(DateTime.now().toString() + ' ' + text);
+    }
+
+    static void t(String text) {
+      print(text);
     }
   }
   ''';
