0cdb0a7a9efeb6d80098f9c3917a719e140afe53
tangpanqing <tangpanqing@qq.com>
2021-06-25 17:04:13 +0800

    .

--- lib/template/bootstrap/db/Db.dart
+++ lib/template/bootstrap/db/Db.dart
@@ -1,15 +1,122 @@
 class Db {
   static String content = '''
-import 'DbColumn.dart';
-import 'DbSqlBuilder.dart';
-import 'DbConnection.dart';
 import 'package:mysql1/mysql1.dart';
+import 'DbConnection.dart';
+import 'DbSqlBuilder.dart';
+import 'DbColumn.dart';
 
 class Db {
   Map<String, dynamic> condition = {};
 
   Db(String tableName) {
     this.condition["table"] = tableName;
+  }
+
+  Future<int> insert(Map<String, dynamic> data) async {
+    this.condition["data"] = data;
+
+    List values = [];
+    String sql = DbSqlBuilder.insert(this.condition, values);
+
+    int id = await DbConnection.insert(sql, values);
+
+    return id;
+  }
+
+  Future<List<Map<String, dynamic>>> select() async {
+    List values = [];
+    String sql = DbSqlBuilder.select(this.condition, values);
+
+    List<Map<String, dynamic>> list = await DbConnection.select(sql, values);
+
+    return list;
+  }
+
+  Future<Map<String, dynamic>> find() async {
+    List<Map<String, dynamic>> list = await this.select();
+
+    if (list.length > 0) {
+      return list[0];
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> update(Map<String, dynamic> data) async {
+    this.condition["set"] = data;
+
+    List<Object> values = [];
+    String sql = DbSqlBuilder.update(this.condition, values);
+
+    int count = await DbConnection.update(sql, values);
+
+    return count;
+  }
+
+  Future<int> delete() async {
+    List<Object> values = [];
+    String sql = DbSqlBuilder.delete(this.condition, values);
+
+    int count = await DbConnection.update(sql, values);
+
+    return count;
+  }
+
+  static Future<Results> query(String sql, [List<Object> values]) async {
+    return await DbConnection.doQuery(sql, values);
+  }
+
+  Future<int> count(String field) async {
+    String fieldName = "count(" + field + ")";
+
+    num n = await _n(fieldName);
+
+    return int.parse(n.toString());
+  }
+
+  Future<double> sum(String field) async {
+    String fieldName = "sum(" + field + ")";
+
+    num n = await _n(fieldName);
+
+    return double.parse(n.toString());
+  }
+
+  Future<double> avg(String field) async {
+    String fieldName = "avg(" + field + ")";
+
+    num n = await _n(fieldName);
+
+    return double.parse(n.toString());
+  }
+
+  Future<num> min(String field) async {
+    String fieldName = "min(" + field + ")";
+    return await _n(fieldName);
+  }
+
+  Future<num> max(String field) async {
+    String fieldName = "max(" + field + ")";
+    return await _n(fieldName);
+  }
+
+  Future<num> _n(String fieldName) async {
+    this.condition["field"] = fieldName;
+    List<Map<String, dynamic>> list = await this.select();
+    if (!list[0].containsKey(fieldName)) return 0;
+    return list[0][fieldName];
+  }
+
+  static void startTrans() async {
+    await DbConnection.startTrans();
+  }
+
+  static void commit() async {
+    await DbConnection.commit();
+  }
+
+  static void rollback() async {
+    await DbConnection.rollback();
   }
 
   Db alias(String s) {
@@ -19,6 +126,11 @@ class Db {
 
   Db where(List<DbColumn> where) {
     this.condition["where"] = where;
+    return this;
+  }
+
+  Db having(List<DbColumn> having) {
+    this.condition["having"] = having;
     return this;
   }
 
@@ -67,11 +179,6 @@ class Db {
     return this;
   }
 
-  Db having(List<DbColumn> having) {
-    this.condition["having"] = having;
-    return this;
-  }
-
   Db distinct(bool s) {
     this.condition["distinct"] = s;
     return this;
@@ -105,108 +212,6 @@ class Db {
   Db force(String s) {
     this.condition["force"] = s;
     return this;
-  }
-
-  Future<int> insert(Map<String, dynamic> data,{bool printSql = false}) async {
-    this.condition["data"] = data;
-
-    List values = [];
-    String sql = DbSqlBuilder.insert(this.condition, values);
-
-    if(printSql){
-      print(sql);
-      print(values);
-    }
-
-    int id = await DbConnection.insert(sql, values);
-
-    return id;
-  }
-
-  Future<int> count({bool printSql = false}) async {
-    List<Map<String, dynamic>> list = await this.select(printSql:printSql);
-
-    return list.length;
-  }
-
-  Future<double> sum(String field,{bool printSql = false}) async {
-    String fieldName = "sum(" + field + ")";
-
-    this.condition["field"] = fieldName;
-    List<Map<String, dynamic>> list = await this.select(printSql:printSql);
-    double d = list[0][fieldName];
-
-    return d;
-  }
-
-  Future<List<Map<String, dynamic>>> select({bool printSql = false}) async {
-    List values = [];
-    String sql = DbSqlBuilder.select(this.condition, values);
-
-    if(printSql){
-      print(sql);
-      print(values);
-    }
-
-    List<Map<String, dynamic>> list = await DbConnection.select(sql, values);
-
-    return list;
-  }
-
-  Future<Map<String, dynamic>> find({bool printSql = false}) async {
-    List<Map<String, dynamic>> list = await this.select(printSql:printSql);
-
-    if (list.length > 0) {
-      return list[0];
-    } else {
-      return null;
-    }
-  }
-
-  Future<int> update(Map<String, dynamic> data,{bool printSql = false}) async {
-    this.condition["set"] = data;
-
-    List<Object> values = [];
-    String sql = DbSqlBuilder.update(this.condition, values);
-
-    if(printSql){
-      print(sql);
-      print(values);
-    }
-
-    int count = await DbConnection.update(sql, values);
-    
-    return count;
-  }
-
-  Future<int> delete({bool printSql = false}) async {
-    List<Object> values = [];
-    String sql = DbSqlBuilder.delete(this.condition, values);
-
-    if(printSql){
-      print(sql);
-      print(values);
-    }
-
-    int count = await DbConnection.update(sql, values);
-
-    return count;
-  }
-
-  static void startTrans() async {
-    await DbConnection.startTrans();
-  }
-
-  static void commit() async {
-    await DbConnection.commit();
-  }
-
-  static void rollback() async {
-    await DbConnection.rollback();
-  }
-
-  static Future<Results> query(String sql, [List<Object> values]) async {
-    return await DbConnection.doQuery(sql, values);
   }
 }
   ''';
