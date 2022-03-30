9da08468e3e8fb9d3b5e63863096220ac3847309
Meng <wangmeng@yichu.net.cn>
2022-03-30 22:55:53 +0800

    将双斜线改为左斜线，这样MAC、linux和windows都可以适应

--- lib/helper/RunHelper.dart
+++ lib/helper/RunHelper.dart
@@ -3,7 +3,7 @@ import 'PackageHelper.dart';
 
 class RunHelper {
   static void run(String type) {
-    var str = 'dart run bin\\' + PackageHelper.getPackageName() + '.dart';
+    var str = 'dart run bin/' + PackageHelper.getPackageName() + '.dart';
     Shell().run(str);
   }
 }
